b1dd2724f30325a944cf59578a064fbfd7dc59b4
verdastelo <verda_stelo@hotmail.com>
2016-04-14 08:00:15 +0530

    Work in progress.

--- deci_to_bin.cpp
+++ deci_to_bin.cpp
@@ -4,17 +4,15 @@
 using namespace std;
 
 int main() {
-  // Converting from decimal to binary involves playing with numbers. For that we'll need some space.
-  // The variables we're going to declare can be our storage spaces.
+  // Converting from decimal to binary requires us to work with numbers. But we can't work with numbers
+  // in vacuum. For that we'll need some space or variables.
+  // The variables we're going to declare will be our storage spaces.
   int deci = 0; // will store the numbers in their decimal representation.
-  // We can start with asking the user to enter a number and storing what he enters in a variable named, deci.
+  // We can start with asking a user to enter a number and storing what he enters in " deci".
   cout << "Enter a number: ";
-  int i; // decimal representation
-  int q; // quotient
-  vector <int> bin; // binary
-  while (cin >> i) {
-      if ((i%2)==0) bin.push_back(0);
-      if ((i%2)==1) bin.push_back(1);
-  }
+  cin >> deci;
+  // Now, we've a number in its decimal representation. What can be done to convert the number into its
+  // binary representation?
+  // 
   return 0;
 }
