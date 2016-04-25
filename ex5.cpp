e6b3c3dc286f9fe259e340f866ad8a3ff943ad2b
verdastelo <verda_stelo@hotmail.com>
2016-04-25 07:15:53 +0530

    Horses, Pigs and Rabbits

--- ex5.cpp
+++ ex5.cpp
@@ -4,6 +4,19 @@
 #include <iostream>
 using namespace std;
 
+int main () {
+
+  for (int h = 0; h < 100; h++)
+    for (int p = 0; p < 100; p++)
+      for (int r = 0; r < 100; r++)
+        if ((h + p + r) == 100)
+          if (((10 * h) + (3 * p) + (0.5 * r)) == 100)
+            cout << "Found one! " << h << " horses " << p << " pigs " << r << " rabbits "  << endl;
+}
+
+/*#include <iostream>
+using namespace std;
+
 int main() {
   double const a_horse_costs = 10.0;
   double const a_pig_costs = 3.0
@@ -18,3 +31,4 @@ int main() {
 
   money_spent = ((a_horse_costs * horses_bought) + (a_pig_costs * pigs_bought) + (a_rabbit_costs * rabbits_bought));
   
+*/
