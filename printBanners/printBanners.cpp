b41ffd179870c472ecd9051c10f4234ada193711
verdastelo <verda_stelo@hotmail.com>
2016-05-01 13:25:22 +0530

    Stuck!

--- printBanners/printBanners.cpp
+++ printBanners/printBanners.cpp
@@ -8,11 +8,10 @@
 using namespace std;
 
 int main() {
-  cout << "Пишите фразу/Skribu frazon: ";
-  string fraza; // фраза
+  cout << "Пишите фразу: ";
+  wstring fraza; // фраза
   getline (cin, fraza);
   cout << "У фразы " << fraza.size() << " букв." << endl;
-  cout << "En la frazo estas " << fraza.size() << " literoj." << endl;
   for (int i = 0; i < fraza.size(); i++) {
     cout << fraza[i] << endl;
   }
