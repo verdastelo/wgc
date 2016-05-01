fc2d4aa339da0b44e89f94fa90506e77fc229069
verdastelo <verda_stelo@hotmail.com>
2016-05-01 13:02:38 +0530

    Ask for input.

--- printBanners/printBanners.cpp
+++ printBanners/printBanners.cpp
@@ -1,11 +1,20 @@
 // printBanners.cpp: Четан Ананд
-// Орисание: Текст на кириллице.
+// Орисание: Создание большие флаги текстом.
 
 #include <iostream>
+#include <string>
+#include <sstream>
 #include "letters.h"
 using namespace std;
 
 int main() {
-  bukvaIA();
+  cout << "Пишите фразу/Skribu frazon: ";
+  string fraza; // фраза
+  getline (cin, fraza);
+  cout << "У фразы " << fraza.size() << " букв." << endl;
+  cout << "En la frazo estas " << fraza.size() << " literoj." << endl;
+  for (int i = 0; i < fraza.size(); i++) {
+    cout << fraza[i] << endl;
+  }
   return 0;
 }
