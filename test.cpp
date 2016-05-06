7f0702921042425745eea4e5ed820331ecc7c9c8
verdastelo <verda_stelo@hotmail.com>
2016-05-06 07:56:28 +0530

    Tests continue.

--- test.cpp
+++ test.cpp
@@ -1,23 +1,15 @@
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
 using namespace std;
 
-int main(void) {
-  double angle = 90.0;
-  cout << "cos(" << angle << ") " << cos (angle) << endl;
-  cout << "sin(" << angle << ") " << sin (angle) << endl;
-  double initialVelocity = 200.0; // 100 metres per second
-  double range = 0.0;
-  double gravity = 9.8; // Acceleration due to gravity
-  double time = 0.0;
-  time = ((2 * (initialVelocity * sin (angle))) / gravity);
-  double velocityX = 0.0; // Velocity along the x-axis
-  velocityX = (initialVelocity * cos (angle));
-  range = velocityX * time;
-  cout << "Range is " << range << endl;
-  cout << "Time is " << time << endl;
-  cout << "Velocity along the x-axis is " << velocityX << endl;
+int main() {
+  int var = 26;
+  int* ptr = &var;
+  int** ptr2 = &ptr;
+  int secondo = var;
+  cout << "var == " << var << endl; // A variable var.
+  cout << "ptr == " << ptr << endl; // The address of var.
+  cout << "ptr2 == " << ptr2 << endl; // The address of the address of var.
+  cout << "&ptr == " << *ptr2 << endl; // The value pointed to by the second address.
+  cout << "secondo == " << secondo << endl;
   return 0;
 }
