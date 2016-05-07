18137bac70a45ba4ac61f5b4e44d7b6d1664bf45
verdastelo <verda_stelo@hotmail.com>
2016-05-08 00:03:19 +0530

    Removed old data

--- test.cpp
+++ test.cpp
@@ -2,24 +2,9 @@
 using namespace std;
 
 int main() {
-  int var = 26;
-  int* ptr = &var;
-  int** ptr2 = &ptr;
-  int secondo = var;
-  cout << "var == " << var << endl; // A variable var.
-  cout << "ptr == " << ptr << endl; // The address of var.
-  cout << "ptr2 == " << ptr2 << endl; // The address of the address of var.
-  cout << "&ptr == " << *ptr2 << endl; // The value pointed to by the second address.
-  cout << "secondo == " << secondo << endl;
-
-  char *myChar;
-  short *myShort;
-  long *myLong;
-  ++myChar;
-  ++myShort;
-  ++myLong;
-  cout << myChar  << endl;
-  cout << myShort << endl;
-  cout << myLong << endl;
+  double num = 100.0;
+  double * ptr = &num;
+  cout << "ptr == " << ptr << endl;
+  cout << "(ptr + 1) == " << (ptr+1) << endl;
   return 0;
 }
