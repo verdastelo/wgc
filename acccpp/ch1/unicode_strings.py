fe07f1aa5c31703597fad0bd9156b9d53735b6b0
verdastelo <verda_stelo@hotmail.com>
2016-06-01 07:40:06 +0530

    len(string) for 5 languages

---
+++ acccpp/ch1/unicode_strings.py
@@ -0,0 +1,11 @@
+esperanto = "ĵurnalo" # ĵurnalo = magazine
+russian = "журнал"    # журнал = magazine
+punjabi = "ਮੈਗਜ਼ੀਨ"     # ਮੈਗਜ਼ੀਨ = magazine
+korean = "일보"       # 일보 = newspaper
+chinese = "学霸"      # 学霸 = nerd (or studious)
+
+print ("len(esperanto) = ", len(esperanto))
+print ("len(russian) = ", len(russian))
+print ("len(punjabi) = ", len(punjabi))
+print ("len(korean) = ", len(korean))
+print ("len(chinese) = ", len(chinese))
