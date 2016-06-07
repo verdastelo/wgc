821002579ae0b914fbb2974a210940f9f3f9020c
verdastelo <verda_stelo@hotmail.com>
2016-06-07 07:36:54 +0530

    Identifiers in Hanguel and Hanja

--- acccpp/ch1/unicode_strings.py
+++ acccpp/ch1/unicode_strings.py
@@ -1,11 +1,13 @@
 esperanto = "ĵurnalo" # ĵurnalo = magazine
 russian = "журнал"    # журнал = magazine
 punjabi = "ਮੈਗਜ਼ੀਨ"     # ਮੈਗਜ਼ੀਨ = magazine
-korean = "일보"       # 일보 = newspaper
-chinese = "学霸"      # 学霸 = nerd (or studious)
+한국어 = "일보"       # 일보 = newspaper
+中文 = "学霸"      # 学霸 = nerd (or studious)
+tamil = "கபாலி"	      # கபாலி = Kabali, a Rajini Kant movie
 
 print ("len(esperanto) = ", len(esperanto))
 print ("len(russian) = ", len(russian))
 print ("len(punjabi) = ", len(punjabi))
-print ("len(korean) = ", len(korean))
-print ("len(chinese) = ", len(chinese))
+print ("len(한국어) = ", len(한국어))
+print ("len(中文) = ", len(中文))
+print ("len(tamil)) = ", len(tamil))
