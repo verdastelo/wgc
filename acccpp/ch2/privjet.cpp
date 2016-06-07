53578e1dd08d28ccf2f0214bc7f73cb9b23a7c35
verdastelo <verda_stelo@hotmail.com>
2016-06-07 07:37:24 +0530

    Test job.

--- acccpp/ch2/privjet.cpp
+++ acccpp/ch2/privjet.cpp
@@ -6,9 +6,22 @@
 
 int main() {
   std::cout << "Please enter your first name: ";
+
   std::string name;
   std::cin >> name;
+
   const std::string greeting = "Hello, " + name + "!";
-  std::cout << greeting << std::endl;
+
+  const int pad = 1;
+  const int rows = (pad * 2) + 3;
+
+  std::cout << std::endl;	// separate the output from the input
+  int r = 0;			// The rows rows of output.
+  while (r != rows) {		// invariant: we've written r rows so far
+    				// write a row for output
+    std::cout << std::endl;
+    ++r;
+  }
+
   return 0;
 }
