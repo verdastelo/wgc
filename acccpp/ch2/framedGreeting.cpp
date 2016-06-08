a89c260a19555ba3ba6b6be69808edc1d1e28830
verdastelo <verda_stelo@hotmail.com>
2016-06-08 07:49:16 +0530

    Figuring out the program's logic.

--- acccpp/ch2/framedGreeting.cpp
+++ acccpp/ch2/framedGreeting.cpp
@@ -11,19 +11,33 @@ int main() {
   // Ask for the person's name.
   cout << "Как вас зовут? Пишите только ваше имя: ";
 
-  // Read the name.
-  string name;
-  cin >> name;
+  // Read the name and store it.
+  // Let's the memory where name is stored be called imja (имя).
+  string imja;
+  cin >> imja;
 
   // Build the message that we intend to write.
-  const std::string greeting = "Saluton, " + name + "!";
+  // The message is of type string.
+  // The message is stored in computer memory.
+  // The part of the memory where the message is stored is called greeting.
+  // "greeting" stores "Saluton, ", imja and an exclamation mark.
+  const std::string greeting = "Hello, " + imja + "!";
 
   // The number of blanks surrounding the greeting.
-  const int pad = 0;
+  // A number 1 is stored somewhere in computer.
+  // The memory occupied by 1 is called "pad."
+  const int pad = 2;
 
   // Total number of rows and columns to write.
+  // A new integer is stored in the computer's memory.
+  // The new integer is pad times two plus three.
   const int rows = pad * 2 + 3;
-  const string::size_type cols = greeting.size() + pad * 2 + 3;
+
+  // A number needs to be stored and interpreted.
+  // The number is the length of a string so instead of int, a new type
+  // is invoked. The type is size_type from the class string.
+  // size_type has properties that allow it to handle very long strings.
+  const string::size_type cols = greeting.size() + pad * 2 + 2;
 
   // Separate the output from the input.
   std::cout << std::endl;
@@ -32,10 +46,12 @@ int main() {
   int r = 0;
 
   // Invariant: We've written r rows so far.
+  // We haven't written any row so far. So the invariant is true in the
+  // beginning.
   for (int r = 0; r != rows; ++r) {
     string::size_type c = 0;
-
     // Invariant: We've written c characters so far in the current row.
+    // No columns have been written so far. So c remains zero.
     while (c != cols) {
       // Is it time to write the greeting?
       if (r == pad + 1 && c == pad + 1) {
@@ -52,3 +68,6 @@ int main() {
     }
     return 0;
 }
+
+// First row is printed.
+// 
