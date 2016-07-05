deca611431082a823268db0858f34beef601b27c
verdastelo <verda_stelo@hotmail.com>
2016-07-05 21:39:18 +0530

    Changes

---
+++ acccpp/ch2/unsigned.cpp
@@ -0,0 +1,17 @@
+// unsigned.cpp: Четан Ананд
+// Описание: Find the range of unsigned ints on this machine.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  unsigned int x;
+  
+
+  for (int i = 0; i < x; i++) {
+    cout << i << ", ";
+  }
+  return 0;
+}  
+
--- gpc/loop.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-  for (int i = 0; i < 1000; i++) {
-    // std::cout << i << std::endl;
-  }
-  return 0;
-}
