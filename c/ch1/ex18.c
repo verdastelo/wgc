1f78914eca29f5690b142ccba9f97ac9c5a58e85
verdastelo <verda_stelo@hotmail.com>
2016-08-15 00:43:01 +0530

    unfinished

---
+++ c/ch1/ex18.c
@@ -0,0 +1,21 @@
+/*
+  Write a program to remove trailing blanks and tabs from each
+  line of input, and to delete entirely blank lines.
+*/
+
+int main (void) {
+  int c, len;
+  last[] = ;
+  
+  while ((c = getline()) != EOF) {
+    if (c == '\b') {
+      blanks(c);
+  } 
+  return 0;
+}
+
+void blanks (s[]) {
+    if (s
+  }
+
+
