5917b9c334a476ba22b81483d5ffd7f3301ccdd1
verdastelo <verda_stelo@hotmail.com>
2016-10-26 08:00:14 +0530

    Experiments

---
+++ exp/enGrammar.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class verbs {
+public:
+  string v1 = run;
+  string v2 = goes;
+  string v3 = eats;
+  string v4 = sleeps;
+  string v5 = drinks;
+}
+
+class nouns {
+public:
+  string n1 = he;
+  string n2 = Okunkwo;
+  string n3 = Hamka;
+  string n4 = Jasdeep;
+  string n5 = Krishan;
+}
+
+int main() {
+{
+  while (cin) {
+    cout << " >";
+    string s = sentence;
+    if (sentence == verbs.v1)
+      cout << "Hi!" << endl;
+ }
+ return 0;
+}
+
+
---
+++ exp/findPrimes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int initial = 2;
+  int final = 1000;
+
+  vector primes = prime[];
+
+  while (initial < final) {
+    for (int i = 2; i <= initial/2; i++) {
+      if (initial%i != 0) 
+        prime.push_back[initial];
+    initial++
+    }
+  }
+  return 0;
+}
+ 
