5b63d12e6a800122bf063bc12e4fd4070e217713
verdastelo <verda_stelo@hotmail.com>
2016-10-27 07:35:20 +0530

    Compiled the program

---
+++ cppPrimer/1/25.cpp
@@ -0,0 +1,29 @@
+// 25.cpp
+// Compile this program.
+// verdastelo (世界语者)
+// ２０１６年１０月２７日
+
+#include <iostream>
+#include "Sales_item.h"
+
+using namespace std;
+
+int main() {
+  Sales_item total;
+  if (cin >> total) {
+    Sales_item trans;
+    while (cin >> trans) { 
+      if (total.isbn() == trans.isbn()) 
+        total += trans;
+      else {
+        cout << total << endl;
+        total = trans;
+      }
+    }
+  cout << total << endl;
+  } else {
+      cerr << "No data!" << endl;
+      return -1;
+     }
+  return 0;
+}
