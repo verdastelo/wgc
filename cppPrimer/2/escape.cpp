803fb47b7f1fdfeceb4986bb86d9192ee8e87640
verdastelo <verda_stelo@hotmail.com>
2016-10-28 07:15:45 +0530

    Bulpis.

---
+++ cppPrimer/2/escape.cpp
@@ -0,0 +1,13 @@
+// escape.cpp
+// Play with escape sequences.
+// verdastelo (世界语者)
+// ２０１６年１０月２８日
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << '\115' << endl;
+  return 0;
+} 
