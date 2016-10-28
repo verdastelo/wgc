ca512b901a1cc03442b728ed1fa37e1d455be964
verdastelo <verda_stelo@hotmail.com>
2016-10-28 07:15:20 +0530

    早, 美, and 学 are converted into ints.

--- cppPrimer/2/unicode.cpp
+++ cppPrimer/2/unicode.cpp
@@ -4,14 +4,17 @@
 // ２０１６年１０月２８日
 
 #include <iostream>
-#include <cwchar>
 
 using namespace std;
 
 int main() {
-  wchar_t zao = '早';
-  cout << zao << endl;
+  wchar_t zao = L'早';		// 早 estas mateno 
+  char16_t xue = u'学';		// 学 estas studado 
+  char32_t mei = U'美';		// 美 estas bela
+  cout << "早 == " << zao << endl;
+  cout << "学 == " << xue << endl;
+  cout << "美 == " << mei << endl;
   return 0;
 }
 
-// Буз успехи! -_- 
+// Без успехи! -_- 
