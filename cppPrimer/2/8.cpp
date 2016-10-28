32e8b0a2d0e796a9ba1f15e4216f40a394b458dc
verdastelo <verda_stelo@hotmail.com>
2016-10-28 07:53:06 +0530

    an incomplete program

---
+++ cppPrimer/2/8.cpp
@@ -0,0 +1,13 @@
+// 8.cpp
+// Print 2M followed by a newline using escape sequences.
+// Print 2 followed by a tab followed by an M and finally a newline using escape sequences. 
+// verdastelo
+// ２０１６年１０月２８日
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  cout << "\62\
+ 
