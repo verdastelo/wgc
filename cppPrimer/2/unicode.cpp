6bb15d5faa0ba77da012c93f41e4f33b46aa5b1a
verdastelo <verda_stelo@hotmail.com>
2016-10-28 06:02:39 +0530

    Inside wchar_t. Doesn't work.

---
+++ cppPrimer/2/unicode.cpp
@@ -0,0 +1,17 @@
+// unicode.cpp
+// Find out how w_char, char16_t, and char32_t work.
+// verdastelo (世界语者)
+// ２０１６年１０月２８日
+
+#include <iostream>
+#include <cwchar>
+
+using namespace std;
+
+int main() {
+  wchar_t zao = '早';
+  cout << zao << endl;
+  return 0;
+}
+
+// Буз успехи! -_- 
