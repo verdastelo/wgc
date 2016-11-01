d33c843accf5ae5efebaba5cd2152757a4a48d1d
verdastelo <verda_stelo@hotmail.com>
2016-11-01 19:55:23 +0530

    cppPrimer/2/13.cpp
    Find i and j

---
+++ cppPrimer/2/13.cpp
@@ -0,0 +1,17 @@
+// 13.cpp
+// Find value of i and j.
+// verdastelo
+// ２０１６年１１月０１日
+
+#include <iostream>
+
+using namespace std;
+
+int i = 42;
+
+int main() {
+  int i = 100;
+  int j = i;
+  cout << "i = " << i << " and j = " << j << endl;
+  return 0;
+}
