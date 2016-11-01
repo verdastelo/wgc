fb09447e9091ae2568b2b9a542bd34c54c0d1602
verdastelo <verda_stelo@hotmail.com>
2016-11-01 19:50:24 +0530

    cppPrimer/2/12.cpp
    Valid and invalid identifiers.

---
+++ cppPrimer/2/12.cpp
@@ -0,0 +1,15 @@
+// 12.cpp
+// Which of the following names are invalid?
+// verdastelo
+// ２０１６年１１月０１日
+
+#include <iostream>
+
+int main() {
+  int double = 3.14; // Invalid.
+  int _; 	     // Unsure.
+  int catch-22;	     // Invalid
+  int 1_or_2 = 1;    // Invalid (This answer was wrong. No starting with numbers.) 
+  double Double = 3.14; // Valid 
+  return 0;
+}
