dfc7d84756a93605c634798755a915e3e0d21603
verdastelo <verda_stelo@hotmail.com>
2016-11-01 19:32:55 +0530

    Exercise 2.9

---
+++ cppPrimer/2/9.cpp
@@ -0,0 +1,14 @@
+// Explain the following definitions.
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+//  cin >> int input_value;  // This won't work because of the space between
+			   // int and input_value
+//  int i = {3.14}; // An error. Truncation isn't allowed. 
+//  double salary = wage = 9999.99; // Illegal. 
+//  int i = 3.14;
+    return 0;
+}
