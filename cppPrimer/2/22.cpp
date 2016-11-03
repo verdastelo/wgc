f6d1086bb74f665dcea024d1728adb8f609559df
verdastelo <verda_stelo@hotmail.com>
2016-11-03 07:57:08 +0530

    Exercise 2.22

---
+++ cppPrimer/2/22.cpp
@@ -0,0 +1,9 @@
+// 22.cpp
+// Explain the following code.
+// verdastelo
+// ２０１６年１１月０３日
+
+Assuming p is a pointer to int, explain the following code.
+
+if (p) // Always true. Because p carries the address. 
+if (*p) // True if int is non-zero.  
