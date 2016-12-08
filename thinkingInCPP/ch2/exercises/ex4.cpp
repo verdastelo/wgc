fcaef2531620715bd81c7f9e9628117ca997b542
verdastelo <verda_stelo@hotmail.com>
2016-12-08 22:43:47 +0530

    Solved

--- thinkingInCPP/ch2/exercises/ex4.cpp
+++ thinkingInCPP/ch2/exercises/ex4.cpp
@@ -9,24 +9,18 @@ using namespace std;
 
 int main() {
     vector <string> words;
-    ifstream in ("/home/anno/Desktop/Practice/wgc/thinkingInCPP/notes");
+    ifstream in ("/home/anno/Desktop/Practice/wgc/thinkingInCPP/ch2/файлы/сандроИзЧегема1");
     string word;
     while (in >> word)
-        words.push_back (word);
-    int count = 0; 				// word count
-    vector <int> counts;
-    string testword; 
-    for (int i = 0; i < words.size(); ++i) {
-        testword = words[i];
-        for (int j = 1; j < words.size(); ++j) {
-            if (testword == words[j]) count++;
-        }
-        counts.push_back (count);
-    }
-    for (int i = 0; i < words.size(); ++i) {
-        for (int i = 0; i < counts.size(); ++i) {
-            cout << words[i] << " == " << counts[i] << endl;
-        }
-    }
+        words.push_back (word);			// each word in the file is stored in words
+
+    cout << "Enter your word >> ";
+    string userword;
+    cin >> userword;
+    int count = 0;
+    for (int i = 0; i < words.size(); ++i)
+        if (words[i] == userword) count++;
+
+    cout << userword << " occurs " << count << " times in the file." << endl; 
     return 0;
 }
