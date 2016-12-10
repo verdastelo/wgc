154635ac4f91a5f2da753666e6ed43b06d7e4149
verdastelo <verda_stelo@hotmail.com>
2016-12-10 14:37:42 +0530

    Solved with help from a solutions guide

--- thinkingInCPP/ch2/exercises/ex7.cpp
+++ thinkingInCPP/ch2/exercises/ex7.cpp
@@ -11,16 +11,25 @@ int main() {
     vector <string> sentences;
     ifstream in ("/home/anno/Desktop/Practice/wgc/thinkingInCPP/ch2/файлы/сандроИзЧегема1");
     string sentence;
-    while (getline(in, sentence))               // getline() reads into sentence until it encounters a '\n'
+    while (getline(in, sentence)) {              // getline() reads into sentence until it encounters a '\n'
+        cout << sentence; 
+        cin.get();
+    }
+    return 0;
+}
+/*
         sentences.push_back (sentence);
 
     char c;
     int count = 0;
     cout << ">> "; 
     while (cin >> c) {
-        if (c == 13) cout << sentences[count] << endl;
+        if (c == '\n') cout << sentences[count] << endl;
         count++;
-        } 
+        cout << ">> ";
+        }
+
+    cout << "total lines published: " << count << endl; 
  
     return 0;
-} 
+}*/ 
