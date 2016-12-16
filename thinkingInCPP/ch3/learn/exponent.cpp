36dd7cb263d615e473c9709c202fb94bb235807e
verdastelo <verda_stelo@hotmail.com>
2016-12-16 07:22:29 +0530

    test exponent doubles

---
+++ thinkingInCPP/ch3/learn/exponent.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+
+int main() {
+    double parsec = 3.26e+5;
+    std::cout << std::hex << parsec << std::endl;
+    std::cout << parsec << std::endl;
+    return 0;
+}
